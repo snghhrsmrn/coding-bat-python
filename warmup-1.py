b7cce7c4bd26b63a604a4902737d4a4d754aa959
snghhrsmrn <snghhrsmrn@gmail.com>
2019-01-07 19:35:00 +0530

    Update warmup-1.py

--- warmup-1.py
+++ warmup-1.py
@@ -62,4 +62,12 @@ def pos_neg(a,b,negative):
     else:
         return (a<0 and b<0) or (a>0 and b<0)
     
-def 
+def not_string(str):
+    """
+    Given a string, return a new string where "not " has been added to the front.
+    However, if the string already begins with "not", return the string unchanged.
+    """
+    if len(str)>=3 and str[:3]=='not':
+        return str
+    else:
+        return "not" + str
