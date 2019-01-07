2a7ef36a14017360c7844734aeb9a3fd78dbb200
snghhrsmrn <snghhrsmrn@gmail.com>
2019-01-07 18:50:08 +0530

    Add files via upload

--- warmup-1.py
+++ warmup-1.py
@@ -1,11 +1,7 @@
-def sleep_in(weekday, vacation):
+def monkey_trouble(a_smile, b_smile):
     """
-    The parameter weekday is True if it is a weekday, and the parameter vacation 
-      is True if we are on vacation. We sleep in if it is not a weekday or we're on 
-      vacation. Return True if we sleep in.
+      We have two monkeys, a and b, and the parameters a_smile and b_smile indicate 
+      if each is smiling. We are in trouble if they are both smiling or if neither 
+      of them is smiling. Return True if we are in trouble. 
     """
-
-    if not weekday or vacation:
-        return True
-    else:
-        return False
+    return a_smile == b_smile
